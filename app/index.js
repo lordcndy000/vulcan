02a052e8d429a32b1c7049a1110c70e9294059f8
lordcndy000 <lordwill.mabalot@gmail.com>
2018-04-09 19:16:00 +0800

    Added landing section

--- app/index.js
+++ app/index.js
@@ -1,11 +1,4 @@
-/**
- * Application entry point
- */
-
-// Load application styles
+import 'normalize.css'
 import 'styles/index.scss'
 
-// ================================
-// START YOUR APP HERE
-// ================================
-console.log('test')
+document.addEventListener('DOMContentLoaded', () => {})
